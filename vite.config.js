b9a4cace9b4920e5f92005ae4c502e804be6d61f
michxymi <michxymi@gmail.com>
2025-02-07 12:37:56 +0000

    setup trail and react

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,18 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
+import { watch } from 'vite-plugin-watch';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: 'resources/js/app.tsx',
             refresh: true,
+        }),
+        react(),
+        watch({
+            pattern: 'routes/*.php',
+            command: 'php artisan trail:generate',
         }),
     ],
 });
