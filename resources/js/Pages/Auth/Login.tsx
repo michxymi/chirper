588fc98a92c9c275be973a4f09d20461d4adcf35
michxymi <michxymi@gmail.com>
2025-02-07 19:51:06 +0000

    update Login page form error title

--- resources/js/Pages/Auth/Login.tsx
+++ resources/js/Pages/Auth/Login.tsx
@@ -47,7 +47,7 @@ export default function Login({
                     {status}
                 </Callout>
             }
-            <InertiaFormErrors title="Authentication Error" errors={errors} icon={RiCloseCircleFill}/>
+            <InertiaFormErrors title="Login Failed" errors={errors} icon={RiCloseCircleFill}/>
             <div className="flex min-h-screen flex-1 flex-col justify-center px-4 lg:px-6">
                 <div className="sm:mx-auto sm:w-full sm:max-w-md">
                     <div className="flex items-center space-x-2.5">
